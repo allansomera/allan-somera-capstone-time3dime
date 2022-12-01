dbe98b5b861f08aab2580cd2ef11a28fcef249e1
Allan Somera <nagal.somera@gmail.com>
2022-11-30 21:17:46 -0800

    got basic migration and seed files to work

--- server/migrations/20221201041447_migration_create_users_table.js
+++ server/migrations/20221201041447_migration_create_users_table.js
@@ -2,14 +2,19 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
-
+exports.up = function (knex) {
+    return knex.schema.createTable("users", (table) => {
+        table.uuid("id").primary()
+        table.string("first_name").notNullable()
+        table.string("last_name").notNullable()
+        table.string("email").notNullable()
+        // table.timestamps(true, true)
+    })
+}
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+    return knex.schema.dropTable("users")
+}
